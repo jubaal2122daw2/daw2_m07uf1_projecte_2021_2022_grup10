a7374e8b3cc2072bdbfe0f2ebb5c9bdaf9ad32ed
jubaal <jubaal2122daw2@protonmail.com>
2021-12-09 19:20:49 +0100

    Arreglo session i constructores bibliotecari

--- Classes/Bibliotecari.php
+++ Classes/Bibliotecari.php
@@ -14,16 +14,49 @@ class Bibliotecari{
     public $salari;
     public $cap;
 
+    public function __construct($nomB,$cognomB,$adrecaB,$emailB,$telefonB,$idB,$passB,$sSocial,$data_contracte,$salari,$cap){
+        $this->nomB = $nomB;
+        $this->cognomB = $cognomB;
+        $this->adrecaB = $adrecaB;
+        $this->emailB = $emailB;
+        $this->telefonB = $telefonB;
+        $this->idB = $idB;
+        $this->passB = $passB;
+        $this->sSocial = $sSocial;
+        $this->data_contracte = $data_contracte;
+        $this->salari = $salari;
+        $this->cap = $cap;
+    }
+
 }
 
 class Cap extends Bibliotecari{
 
-        
+    function __construct($nomB,$cognomB,$adrecaB,$emailB,$telefonB,$idB,$passB,$sSocial,$data_contracte,$salari,$cap) {
+        parent::__construct($nomB,$cognomB,$adrecaB,$emailB,$telefonB,$idB,$passB,$sSocial,$data_contracte,$salari,$cap);
+    }
+
+    public function getIdCap(){
+        return $this-> idU;
+    }
+
+    public function getNomCap(){
+        return $this-> nomU;
+    }
 }
 
 class Treballador extends Bibliotecari{
+    function __construct($nomB,$cognomB,$adrecaB,$emailB,$telefonB,$idB,$passB,$sSocial,$data_contracte,$salari,$cap) {
+        parent::__construct($nomB,$cognomB,$adrecaB,$emailB,$telefonB,$idB,$passB,$sSocial,$data_contracte,$salari,$cap);
+    }
 
-    
+    public function getIdTreb(){
+        return $this-> idB;
+    }
+
+    public function getNomTreb(){
+        return $this-> nomB;
+    }
 }
 
 ?>
--- sessio_u.php
+++ sessio_u.php
@@ -1,5 +1,6 @@
 <?php
     include("Classes/Usuari.php");
+    include("Classes/Bibliotecari.php");
 
     $user_file = "./ficheros/usuaris.csv";
     $fitxer = fopen($user_file,"r") or die ("No s'ha pogut crear fitxer");
@@ -8,10 +9,19 @@
         while (($datos = fgetcsv($fitxer,0,",")) !== FALSE){ //devuelve un array indexado 
             for($i = 0 ; $i < count($datos) ; $i++){
                 if($datos[5]==$_POST["usuari"] && $datos[6]==$_POST["ctsnya"]){
-                    session_start();
-                    $_SESSION["check_user"]= new Usuari($datos[0],$datos[1],$datos[2],$datos[3],$datos[4],$datos[5],$datos[6],$datos[7],$datos[8],$datos[9]);
-                    //session es una array que contiene este objeto como un parametro. 
-                    echo "Has entrat en la sessió";
+                    $lastChar = substr($datos[5], -1);
+                    if($lastChar=="U"){
+                        session_start();
+                        $_SESSION["check_user"]= new Usuari($datos[0],$datos[1],$datos[2],$datos[3],$datos[4],$datos[5],$datos[6],$datos[7],$datos[8],$datos[9]); 
+                    }//session es una array que contiene este objeto como un parametro. 
+                    if($lastChar=="B"){
+                        session_start();
+                        $_SESSION["check_biblio"]= new Treballador($datos[0],$datos[1],$datos[2],$datos[3],$datos[4],$datos[5],$datos[6],$datos[7],$datos[8],$datos[9],$datos[10],$datos[11]);
+                    }
+                    if($lastChar=="C"){
+                        session_start();
+                        $_SESSION["check_biblioc"]= new Cap($datos[0],$datos[1],$datos[2],$datos[3],$datos[4],$datos[5],$datos[6],$datos[7],$datos[8],$datos[9],$datos[10],$datos[11]);
+                    }
                     break;
                 }
             }
@@ -44,7 +54,27 @@
                 $in_user = file_get_contents("inusuaris.html");
                 echo $in_user;
             }
-            elseif(!isset($_SESSION["check_user"])){
+            if(isset($_SESSION["check_biblio"])){ //si un usuario es instanciado en la sesion.
+                echo 
+                "<div class='lateral'> 
+                <b>ID de sessió:</b> " .session_id()."<br>".
+                "<b>ID d'Usuari: </b>".$_SESSION["check_biblio"] -> getIdTreb()."<br>".
+                "<a href='logout.php'>Cerrar Sesión</a>".
+                "</div>";
+                $in_biblio = file_get_contents("inbibliotecaris.html");
+                echo $in_biblio;
+            }
+            if(!isset($_SESSION["check_user"])&&!isset($_SESSION["check_biblio"])&&isset($_SESSION["check_biblioc"])){ //si un usuario es instanciado en la sesion.
+                echo 
+                "<div class='lateral'> 
+                <b>ID de sessió:</b> " .session_id()."<br>".
+                "<b>ID d'Usuari: </b>".$_SESSION["check_biblioc"] -> getIdCap()."<br>".
+                "<a href='logout.php'>Cerrar Sesión</a>".
+                "</div>";
+                $in_biblioc = file_get_contents("inbibliotecariscap.html");
+                echo $in_biblioc;
+            }
+            elseif(!isset($_SESSION["check_user"])&&!isset($_SESSION["check_biblio"])&&!isset($_SESSION["check_biblioc"])){
                 echo "Usuari o contrassenya incorrectes";
             }
         ?>
